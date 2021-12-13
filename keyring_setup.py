c485fe5e89622bd28657ac9a73a7498b552b1758
Dracohydrus <dracokris123@gmail.com>
2021-12-13 13:48:37 -0500

    Added more comments to keyring setup

--- keyring_setup.py
+++ keyring_setup.py
@@ -1,12 +1,13 @@
 import keyring
 
-# uncomment as needed. Use this file to run and create keyring values before running the main.py function
+# Uncomment as needed. Use this file to run and create keyring values before running the main.py function
 
+# Creating Keyring Passwords
 # keyring.set_password("newegg_login","email","")
 # keyring.set_password("newegg_login","password","")
 # keyring.set_password("newegg_login","cvv","")
 
-# test keyring was created
+# Testing Keyring was created. This part doesn't need to be used, just use to make sure a keyring was set up if you are getting errors
 # print(keyring.get_password("newegg_login","email"))
 # print(keyring.get_password("newegg_login","password"))
 # print(keyring.get_password("newegg_login","cvv"))
